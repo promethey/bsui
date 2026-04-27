3ebba15b27608277044f152d3905c1fdfe85d93c
promethey <sedelkovegor@gmail.com>
2026-04-28 04:53:45 +0500

    add: new helper is for better check typeof

---
+++ src/helpers/is/index.js
@@ -0,0 +1,1 @@
+export { is } from "./is";
---
+++ src/helpers/is/is.js
@@ -0,0 +1,54 @@
+/**
+ * Function for check value type
+ * use strict mode for check empty string, empty object and empty array
+ *
+ * @example
+ * is("string", "") // true
+ * is("string", " ") // true
+ * is("string", " ", { strict: true }) // false
+ *
+ * is("number", 123) // true
+ * is("number", 0) // true
+ *
+ * is("object", {}) // true
+ * is("object", {}, { strict: true }) // false
+ * is("object", { value: "" }, { strict: true }) // true
+ *
+ * @param {any} type - "string", "number", "object", "array"
+ * @param {any} value - all of types
+ * @param {boolean} option.notEmpty - not empty check value (work for string, object and array)
+ *
+ * @returns {boolean} result
+ */
+export function is(type, value, options = { notEmpty: false }) {
+  switch (type) {
+    case "string": {
+      if (typeof value !== type) return false;
+      if (options.notEmpty && value.trim().length === 0) return false;
+
+      return true;
+    }
+
+    case "object": {
+      if (typeof value !== type && !value && Array.isArray(value)) return false;
+      if (options.notEmpty && Object.keys(value).length === 0) return false;
+
+      return true;
+    }
+
+    case "array": {
+      if (!Array.isArray(value)) return false;
+      if (options.notEmpty && value.length === 0) return false;
+
+      return true;
+    }
+
+    case "number": {
+      return typeof value === type && !Number.isNaN(value);
+    }
+
+    default: {
+      return typeof value === type;
+    }
+  }
+}
