d903773619cb05527aa3254e0dfa1ea7353ae39a
promethey <sedelkovegor@gmail.com>
2026-04-28 02:30:16 +0500

    refactor: shadow util

--- src/utils/shadow/shadow.js
+++ src/utils/shadow/shadow.js
@@ -4,6 +4,20 @@ const SHADOW_CLASS_NAME = "shadow";
 
 const SHADOW_VALUES = ["none", "sm", true, "lg"];
 
+/**
+ * Shadow function
+ *
+ * @see {@link https://getbootstrap.com/docs/5.1/utilities/shadows/}
+ *
+ * @examples
+ * shadow(true) // 'shadow' like a regular shadow
+ * shadow("none") // 'shadow-none'
+ * shadow("sm") // 'shadow-sm'
+ *
+ * @param {string|boolean} value
+ *
+ * @returns {string} classnames
+ */
 export function shadow(value) {
   if (!value) return "";
 
