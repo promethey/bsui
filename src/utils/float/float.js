57e6df79ed7e46f1e5160ac2ee8628f2618cd1c4
promethey <sedelkovegor@gmail.com>
2026-04-28 06:26:08 +0500

    refactor: add is for float

--- src/utils/float/float.js
+++ src/utils/float/float.js
@@ -1,4 +1,5 @@
 import { classnames as cs } from "helpers/classnames";
+import { is } from "helpers/is";
 
 const FLOAT_CLASS_NAME = "float";
 const FLOAT_VALUES = ["start", "end", "none"];
@@ -21,19 +22,14 @@ export function float(value) {
   if (!value) return "";
 
   // String
-  if (typeof value === "string" && value.trim()) {
+  if (is("string", value, { notEmpty: true })) {
     if (FLOAT_VALUES.includes(value.trim())) {
       return cs(FLOAT_CLASS_NAME, value.trim());
     }
   }
 
   // Object
-  if (
-    typeof value === "object" &&
-    value &&
-    !Array.isArray(value) &&
-    Object.keys(value).length > 0
-  ) {
+  if (is("object", value, { notEmpty: true })) {
     const floatFilterUnsupportedValues = Object.entries(value).filter(
       ([breakpoint, displayValue]) =>
         FLOAT_BREAKPOINTS.includes(breakpoint) &&
