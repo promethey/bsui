28b64265b45c6478cebe5d6d79b1ec75beb26673
promethey <sedelkovegor@gmail.com>
2026-05-28 00:35:17 +0500

    refactor: improve Row

--- src/components/Row/Row.jsx
+++ src/components/Row/Row.jsx
@@ -3,7 +3,7 @@ import cn from "classnames";
 import { Prime } from "components";
 import { gutterResolver, columnsResolver } from "./utils";
 
-const ROW_CLASS_NAME = "row";
+const BASE_CLASS_NAME = "row";
 
 const propTypes = {
   /**
@@ -22,22 +22,22 @@ const propTypes = {
   className: PropTypes.oneOfType([PropTypes.object, PropTypes.string]),
 
   /**
-   * Sets row columns
+   * Defines the number of columns in the row layout
    */
   cols: PropTypes.oneOf([1, 2, 3, 4, 5, 6, 7, 8, 9, 11, 12, "auto"]),
 
   /**
-   * Sets gaps between columns
+   * Controls the gap between columns (both axes)
    */
   g: PropTypes.oneOf([0, 1, 2, 3, 4, 5]),
 
   /**
-   * Sets gaps between columns axis-X
+   * Controls horizontal gap between columns
    */
   gx: PropTypes.oneOf([0, 1, 2, 3, 4, 5]),
 
   /**
-   * Sets gaps between columns axis-Y
+   * Controls vertical gap between columns
    */
   gy: PropTypes.oneOf([0, 1, 2, 3, 4, 5]),
 };
@@ -53,44 +53,57 @@ const defaultProps = {
 };
 
 /**
- * Row component
+ * Layout component that defines a horizontal
+ * flex/grid row used to structure columns
+ * in a responsive grid system.
  *
  * @see {@link Prime}
  * @see {@link https://getbootstrap.com/docs/5.1/layout/grid/}
  *
  * @example
- * <Row>.row</Row>
+ * <Row flex={{ align: "start" }}>
+ *  .row .align-items-start
+ * </Row>
  *
  * @example
- * <Row flex={{ align: "start" }}>.row .align-items-start</Row>
+ * <Row cols={3} g={4}>
+ *  .row .row-cols-3 .g-4
+ * </Row>
  *
  * @example
- * <Row cols={3} g={4}>.row .row-cols-3 .g-4</Row>
- *
- * @example
- * <Row cols={{ xs: 3, md: 4 }}>.row .row-cols-3 .row-cols-md-4</Row>
+ * <Row cols={{ xs: 3, md: 4 }}>
+ *  .row .row-cols-3 .row-cols-md-4
+ * </Row>
  *
  * @typedef {import("../Prime/Prime").PrimeProps} PrimeProps
  * @typedef {import("./utils/columns").ColumnsObject} ColumnsObject
  * @typedef {import("./utils/gutter").GutterObject} GutterObject
  *
  * @typedef {object} RowOwnProps
- * @property {1|2|3|4|5|6|7|8|9|10|11|12|"auto"|ColumnsObject} [cols] - Sets row columns
- * @property {0|1|2|3|4|5|GutterObject} [g] - Sets gaps between columns
- * @property {0|1|2|3|4|5|GutterObject} [gx] - Sets gaps between columns axis-X
- * @property {0|1|2|3|4|5|GutterObject} [gy] - Sets gaps between columns axis-Y
+ *
+ * @property {1|2|3|4|5|6|7|8|9|10|11|12|"auto"|ColumnsObject} [cols]
+ * Defines the number of columns in the row layout.
+ *
+ * @property {0|1|2|3|4|5|GutterObject} [g]
+ * Controls the gap between columns (both axes).
+ *
+ * @property {0|1|2|3|4|5|GutterObject} [gx]
+ * Controls horizontal gap between columns.
+ *
+ * @property {0|1|2|3|4|5|GutterObject} [gy]
+ * Controls vertical gap between columns.
  *
  * @typedef {RowOwnProps & PrimeProps} RowProps
  * @param {RowProps} props
  *
- * @author Sedelkov Egor <sedelkovegor@gmail.com>
+ * @author Sedelkov Egor [promethey] <sedelkovegor@gmail.com>
  * @version 1.0.0
  */
 function Row(props) {
   const { style, children, className, cols, g, gx, gy, ...rest } = props;
 
   const classes = cn(
-    ROW_CLASS_NAME,
+    BASE_CLASS_NAME,
     columnsResolver(cols),
     gutterResolver({ g, gx, gy }),
     className,
